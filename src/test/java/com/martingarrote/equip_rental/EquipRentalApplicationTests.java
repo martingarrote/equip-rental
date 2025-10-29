7dd870ff358c66b66a32fbd712e0c25ba7a33b30
martingarrote <101750373+martingarrote@users.noreply.github.com>
2025-10-28 21:09:13 -0300

    first commit

---
+++ src/test/java/com/martingarrote/equip_rental/EquipRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martingarrote.equip_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
