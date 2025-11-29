3b09db4c4e5a2931a411db054613f5efd78c7fd8
martingarrote <101750373+martingarrote@users.noreply.github.com>
2025-11-29 18:41:09 -0300

    atualizar método para obter usuário logado

--- src/main/java/com/martingarrote/equip_rental/infrastructure/security/SecurityUtils.java
+++ src/main/java/com/martingarrote/equip_rental/infrastructure/security/SecurityUtils.java
@@ -1,12 +1,33 @@
 package com.martingarrote.equip_rental.infrastructure.security;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import com.martingarrote.equip_rental.domain.user.UserService;
+import com.martingarrote.equip_rental.infrastructure.exception.ErrorMessage;
+import com.martingarrote.equip_rental.infrastructure.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
+@RequiredArgsConstructor
 public class SecurityUtils {
 
-    public String getCurrentUserEmail(UserDetails userDetails) {
-        return userDetails.getUsername();
+    private final UserService userService;
+
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ServiceException(ErrorMessage.UNAUTHORIZED);
+        }
+
+        return authentication.getName();
+    }
+
+    public UUID getCurrentUserId() {
+        var email = getCurrentUserEmail();
+        return userService.findEntityByEmail(email).getId();
     }
 }
